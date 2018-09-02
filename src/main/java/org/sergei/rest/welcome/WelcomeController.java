a4815eb8f8c24ac4bc7d4a30b1a2b8da289de6eb
Sergei <sergei.visotsky@gmail.com>
2018-09-02 21:19:48 +0300

    Config files change, welcome file update, standard servlet removed

---
+++ src/main/java/org/sergei/rest/welcome/WelcomeController.java
@@ -0,0 +1,17 @@
+package org.sergei.rest.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping
+public class WelcomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String welcome() {
+        return "REST";
+    }
+}
