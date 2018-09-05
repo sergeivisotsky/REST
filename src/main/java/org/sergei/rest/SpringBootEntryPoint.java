5b71c2e184f7f61f8261a0fcbc7506a367ad7df6
Sergei <sergei.visotsky@gmail.com>
2018-09-05 18:41:11 +0300

    CORS configuration added

--- src/main/java/org/sergei/rest/SpringBootEntryPoint.java
+++ src/main/java/org/sergei/rest/SpringBootEntryPoint.java
@@ -6,10 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@ImportResource("classpath:REST_API-servlet.xml")
 public class SpringBootEntryPoint extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
