3deb958fb96e7ae811afad606d150c44bfb31d1a
Sergei <sergei.visotsky@gmail.com>
2018-09-06 06:59:37 +0300

    Mapping change

--- src/main/java/org/sergei/rest/welcome/Welcome.java
+++ src/main/java/org/sergei/rest/welcome/Welcome.java
@@ -1,17 +1,17 @@
 package org.sergei.rest.welcome;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import springfox.documentation.annotations.ApiIgnore;
 
 @ApiIgnore
 @Controller
-@RequestMapping
+@RequestMapping("/")
 public class Welcome {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping
     @ResponseBody
     public String welcome() {
         return "REST";
