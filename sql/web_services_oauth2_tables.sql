1403b56b2205fe51007b74273b9b3bb565d83449
Sergei <sergei.visotsky@gmail.com>
2018-09-18 16:51:21 +0300

    Migration to the MySQL

--- sql/web_services_oauth2_tables.sql
+++ sql/web_services_oauth2_tables.sql
@@ -1,5 +1,8 @@
-drop table if exists oauth_client_details;
-create table oauth_client_details (
+drop database if exists web_services_oauth;
+CREATE DATABASE web_services_oauth;
+
+drop table if exists web_services_oauth.oauth_client_details;
+create table web_services_oauth.oauth_client_details (
   client_id               VARCHAR(256) PRIMARY KEY,
   resource_ids            VARCHAR(256),
   client_secret           VARCHAR(256),
@@ -11,83 +14,53 @@ create table oauth_client_details (
   refresh_token_validity  INTEGER,
   additional_information  VARCHAR(4096),
   autoapprove             VARCHAR(256)
-)
-WITH (
-OIDS = FALSE
 );
-ALTER TABLE oauth_client_details
-  OWNER TO service;
 
-drop table if exists oauth_client_token;
-create table oauth_client_token (
+drop table if exists web_services_oauth.oauth_client_token;
+create table web_services_oauth.oauth_client_token (
   token_id          VARCHAR(256),
-  token             BYTEA,
+  token             BLOB,
   authentication_id VARCHAR(256) PRIMARY KEY,
   user_name         VARCHAR(256),
   client_id         VARCHAR(256)
-)
-WITH (
-OIDS = FALSE
 );
-ALTER TABLE oauth_client_token
-  OWNER TO service;
 
-drop table if exists oauth_access_token;
-create table oauth_access_token (
+drop table if exists web_services_oauth.oauth_access_token;
+create table web_services_oauth.oauth_access_token (
   token_id          VARCHAR(256),
-  token             BYTEA,
+  token             BLOB,
   authentication_id VARCHAR(256),
   user_name         VARCHAR(256),
   client_id         VARCHAR(256),
-  authentication    BYTEA,
+  authentication    BLOB,
   refresh_token     VARCHAR(256)
-)
-WITH (
-OIDS = FALSE
 );
-ALTER TABLE oauth_access_token
-  OWNER TO service;
 
-drop table if exists oauth_refresh_token;
-create table oauth_refresh_token (
+drop table if exists web_services_oauth.oauth_refresh_token;
+create table web_services_oauth.oauth_refresh_token (
   token_id       VARCHAR(256),
-  token          BYTEA,
-  authentication BYTEA
-)
-WITH (
-OIDS = FALSE
+  token          BLOB,
+  authentication BLOB
 );
-ALTER TABLE oauth_refresh_token
-  OWNER TO service;
 
-drop table if exists oauth_code;
-create table oauth_code (
+drop table if exists web_services_oauth.oauth_code;
+create table web_services_oauth.oauth_code (
   code           VARCHAR(256),
-  authentication BYTEA
-)
-WITH (
-OIDS = FALSE
+  authentication BLOB
 );
-ALTER TABLE oauth_code
-  OWNER TO service;
 
-drop table if exists oauth_approvals;
-create table oauth_approvals (
+drop table if exists web_services_oauth.oauth_approvals;
+create table web_services_oauth.oauth_approvals (
   userId         VARCHAR(256),
   clientId       VARCHAR(256),
   scope          VARCHAR(256),
   status         VARCHAR(10),
   expiresAt      TIMESTAMP,
   lastModifiedAt TIMESTAMP
-)
-WITH (
-OIDS = FALSE
 );
-ALTER TABLE oauth_approvals
-  OWNER TO service;
 
-drop table if exists client_details;
-create table client_details (
+drop table if exists web_services_oauth.client_details;
+create table web_services_oauth.client_details (
   appId                  VARCHAR(256) PRIMARY KEY,
   resourceIds            VARCHAR(256),
   appSecret              VARCHAR(256),
@@ -99,9 +72,4 @@ create table client_details (
   refresh_token_validity INTEGER,
   additionalInformation  VARCHAR(4096),
   autoApproveScopes      VARCHAR(256)
-)
-WITH (
-OIDS = FALSE
 );
-ALTER TABLE client_details
-  OWNER TO service;
--- sql/web_services_tables.sql
+++ sql/web_services_tables.sql
@@ -1,54 +1,31 @@
 ﻿drop database if exists web_services;
-CREATE DATABASE web_services
-WITH OWNER = service
-ENCODING = 'UTF8'
-TABLESPACE = pg_default
-LC_COLLATE = 'English_United States.1252'
-LC_CTYPE = 'English_United States.1252'
-CONNECTION LIMIT = -1;
+CREATE DATABASE web_services;
 
-drop table if exists customers;
-create table customers (
-  customer_id bigserial not null primary key,
+drop table if exists web_services.customers;
+create table web_services.customers (
+  customer_id bigint auto_increment not null unique primary key,
   first_name  varchar(50),
   last_name   varchar(50),
   age         integer
-)
-
-WITH (
-OIDS = FALSE
 );
-ALTER TABLE public.customers
-  OWNER TO service;
 
-drop table if exists orders;
-create table orders (
-  order_id    bigserial not null primary key,
-  customer_id bigserial not null references customers (customer_id) on delete cascade,
-  trans_id    bigserial not null,
+drop table if exists web_services.orders;
+create table web_services.orders (
+  order_id    bigint auto_increment not null unique primary key,
+  customer_id bigint not null references customers (customer_id) on delete cascade,
+  trans_id    bigint not null,
   good        varchar(50),
   good_weight double precision,
   price       double precision
-)
-
-WITH (
-OIDS = FALSE
 );
-ALTER TABLE public.orders
-  OWNER TO service;
 
-drop table if exists photos;
-CREATE TABLE photos
+drop table if exists web_services.photos;
+CREATE TABLE web_services.photos
 (
-  photo_id    bigserial NOT NULL primary key,
-  customer_id bigserial not null references customers (customer_id) on delete cascade,
+  photo_id    bigint auto_increment NOT NULL unique primary key,
+  customer_id bigint not null references customers (customer_id) on delete cascade,
   file_name   varchar(100),
   file_url    varchar(150),
   file_type   varchar(50),
   file_size   integer
-)
-WITH (
-OIDS = FALSE
 );
-ALTER TABLE public.photos
-  OWNER TO service;
