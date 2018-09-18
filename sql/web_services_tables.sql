a7555edd2dd4866a3c5ed0e3d11677f908a8435e
Sergei <sergei.visotsky@gmail.com>
2018-09-18 07:50:16 +0300

    Minor changes

--- sql/web_services_tables.sql
+++ sql/web_services_tables.sql
@@ -8,31 +8,31 @@ LC_CTYPE = 'English_United States.1252'
 CONNECTION LIMIT = -1;
 
 drop table if exists customers;
-create table customers(
-   customer_id bigserial not null primary key,
-   first_name varchar(50),
-   last_name varchar(50),
-   age integer
+create table customers (
+  customer_id bigserial not null primary key,
+  first_name  varchar(50),
+  last_name   varchar(50),
+  age         integer
 )
 
 WITH (
-  OIDS=FALSE
+OIDS = FALSE
 );
 ALTER TABLE public.customers
   OWNER TO service;
 
 drop table if exists orders;
-create table orders(
-   order_id bigserial not null primary key,
-   customer_id bigserial not null references customers(customer_id) on delete cascade,
-   trans_id bigserial not null,
-   good varchar(50),
-   good_weight double precision,
-   price double precision
+create table orders (
+  order_id    bigserial not null primary key,
+  customer_id bigserial not null references customers (customer_id) on delete cascade,
+  trans_id    bigserial not null,
+  good        varchar(50),
+  good_weight double precision,
+  price       double precision
 )
 
 WITH (
-  OIDS=FALSE
+OIDS = FALSE
 );
 ALTER TABLE public.orders
   OWNER TO service;
@@ -40,15 +40,15 @@ ALTER TABLE public.orders
 drop table if exists photos;
 CREATE TABLE photos
 (
-  photo_id bigserial NOT NULL primary key,
-  customer_id bigserial not null references customers(customer_id) on delete cascade,
-  file_name varchar(100),
-  file_url varchar(150),
-  file_type varchar(50),
-  file_size integer
+  photo_id    bigserial NOT NULL primary key,
+  customer_id bigserial not null references customers (customer_id) on delete cascade,
+  file_name   varchar(100),
+  file_url    varchar(150),
+  file_type   varchar(50),
+  file_size   integer
 )
 WITH (
-OIDS=FALSE
+OIDS = FALSE
 );
 ALTER TABLE public.photos
   OWNER TO service;
