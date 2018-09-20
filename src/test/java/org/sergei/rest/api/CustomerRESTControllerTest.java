6e2b400d5ce14390be7872258eacd4920b8350c8
Sergei <sergei.visotsky@gmail.com>
2018-09-20 21:30:30 +0300

    Mock MVC integration test development started for the Spring REST Docs

--- src/test/java/org/sergei/rest/api/CustomerRESTControllerTest.java
+++ src/test/java/org/sergei/rest/api/CustomerRESTControllerTest.java
@@ -1,50 +1,47 @@
 package org.sergei.rest.api;
 
-import org.codehaus.jackson.map.ObjectMapper;
 import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.sergei.rest.model.Customer;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.sergei.rest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.Mockito.when;
+import javax.annotation.Resource;
+
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ConfigurationProperties("classpath:src/test/resources/REST_API-servlet-test.xml")
+//@ConfigurationProperties("classpath:src/test/resources/REST_API-servlet-test.xml")
+@ContextConfiguration("classpath:REST_API-servlet-test.xml")
 @WebAppConfiguration
-public //@AutoConfigureRestDocs
-class CustomerRESTControllerTest {
+public class CustomerRESTControllerTest {
 
     /*@Rule
     public RestDocumentation restDocumentation = new RestDocumentation("target/generated-snippets");*/
-
-    @MockBean
-    private CustomerService customerService;
-
-    @Autowired
+    @Resource
     private WebApplicationContext context;
 
-    @Autowired
     private MockMvc mockMvc;
+
+    // FIXME: Not a Mock
+    @MockBean
+    @Autowired
+    private CustomerService customerService;
 
     @Before
     public void setUp() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-//                .apply(documentationConfiguration(this.restDocumentation))
-                .build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
     }
 
     @After
