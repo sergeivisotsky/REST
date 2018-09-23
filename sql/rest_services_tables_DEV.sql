c815aa4a0b71db136aa9545c3d2a1c0c78b42d7a
Sergei <sergei.visotsky@gmail.com>
2018-09-23 09:06:57 +0300

    backup data in sql code added

--- sql/rest_services_tables_DEV.sql
+++ sql/rest_services_tables_DEV.sql
@@ -1,9 +1,3 @@
-/*
-*
-* BACKUP
-*
- */
-
 DROP DATABASE IF EXISTS rest_services;
 CREATE DATABASE rest_services /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 
@@ -44,7 +38,3 @@ CREATE TABLE rest_services.photos
   REFERENCES rest_services.customers (customer_id)
   ON UPDATE NO ACTION ON DELETE CASCADE
 );
-
-INSERT INTO rest_services.orders (order_id, customer_id, trans_id, product, product_weight, price) VALUES(1, 1, 2412354235, "car", 200, 250000);
-INSERT INTO rest_services.orders (order_id, customer_id, trans_id, product, product_weight, price) VALUES(2, 1, 5236346, "cat", 1, 300);
-INSERT INTO rest_services.orders (order_id, customer_id, trans_id, product, product_weight, price) VALUES(3, 1, 63246, "flowers", 2, 1.5);
