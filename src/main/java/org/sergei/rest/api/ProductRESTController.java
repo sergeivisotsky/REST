6f767606a7aeedf4850b8525bae54107f1244109
Sergei <sergei.visotsky@gmail.com>
2018-09-24 11:48:28 +0300

    Field ranamed, code refactored, product DAO, Service and REST controller added all of them under construction

--- src/main/java/org/sergei/rest/api/ProductRESTController.java
+++ src/main/java/org/sergei/rest/api/ProductRESTController.java
@@ -4,5 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductRESTController {
-    // TODO
+    // TODO: Find all products
+
+    // TODO: Find product by product code
+
+    // TODO: Create new product
+
+    // TODO: Update product by code
+
+    // TODO: Delete product by code
 }
--- src/main/java/org/sergei/rest/dao/ProductDAO.java
+++ src/main/java/org/sergei/rest/dao/ProductDAO.java
@@ -4,5 +4,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductDAO {
-    // TODO
+    // TODO: Find all products
+
+    // TODO: Find product by product code
+
+    // TODO: Create new product
+
+    // TODO: Update product by code
+
+    // TODO: Delete product by code
 }
