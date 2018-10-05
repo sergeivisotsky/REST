9f66020423244e2b1f1fdb19ad95c2c96d978cdc
Sergei <sergei.visotsky@gmail.com>
2018-10-05 15:42:03 +0300

    Comments removed

--- src/main/java/org/sergei/rest/swagger/SwaggerConfig.java
+++ src/main/java/org/sergei/rest/swagger/SwaggerConfig.java
@@ -20,10 +20,6 @@ import java.util.List;
 
 import static org.springframework.security.oauth2.provider.token.AccessTokenConverter.CLIENT_ID;
 
-/**
- * FIXME: Swagger stopped working
- * FIXME: Message - Unable to infer base url.
- */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
