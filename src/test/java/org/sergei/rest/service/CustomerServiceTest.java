b1a9d10e2ca36137adfa5fd6c46742bf5ebd5c76
Sergei <sergei.visotsky@gmail.com>
2018-10-07 19:10:28 +0300

    Configuration renamed

--- src/test/java/org/sergei/rest/service/CustomerServiceTest.java
+++ src/test/java/org/sergei/rest/service/CustomerServiceTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/REST_API-servlet.xml")
+@ContextConfiguration("file:../../../../../../main/webapp/WEB-INF/applicationContext.xml")
 public class CustomerServiceTest {
 
     @Autowired
