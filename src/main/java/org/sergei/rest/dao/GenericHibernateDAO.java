c5a986ac0ac9d7c802701862ff2e365b028428ae
Sergei <sergei.visotsky@gmail.com>
2018-10-11 21:04:36 +0300

    Code format performed

--- src/main/java/org/sergei/rest/dao/GenericHibernateDAO.java
+++ src/main/java/org/sergei/rest/dao/GenericHibernateDAO.java
@@ -1,6 +1,6 @@
 package org.sergei.rest.dao;
 
-import      org.hibernate.Session;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
