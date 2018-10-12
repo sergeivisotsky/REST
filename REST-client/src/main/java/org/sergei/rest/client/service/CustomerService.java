ffa8d61c6086d368dd79b7347349a610a1b41cd1
Sergei <sergei.visotsky@gmail.com>
2018-10-12 17:33:55 +0300

    JS clients moved to the module, Code refactoring

--- REST-client/src/main/java/org/sergei/rest/client/service/CustomerService.java
+++ REST-client/src/main/java/org/sergei/rest/client/service/CustomerService.java
@@ -1,8 +1,6 @@
 package org.sergei.rest.client.service;
 
 import org.sergei.rest.client.pojo.CustomerVO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -16,8 +14,6 @@ public class CustomerService {
 
     private static final String REQUEST_URI_GET_BY_CUSTOMER_NUMBER = "http://localhost:8080/rest/api/v1/customers/{customerNumber}";
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
     private final RestTemplate restTemplate;
     private final HttpHeaders httpHeaders;
 
@@ -27,14 +23,8 @@ public class CustomerService {
         this.httpHeaders = httpHeaders;
     }
 
-
     public CustomerVO getCustomerByNumber(Long customerNumber) {
-
         httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        CustomerVO customerVO = restTemplate.getForObject(REQUEST_URI_GET_BY_CUSTOMER_NUMBER, CustomerVO.class, customerNumber);
-
-        logger.info(customerVO != null ? customerVO.toString() : null);
-
-        return customerVO;
+        return restTemplate.getForObject(REQUEST_URI_GET_BY_CUSTOMER_NUMBER, CustomerVO.class, customerNumber);
     }
 }
