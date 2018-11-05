1544844af3a060066a92ce74144d65f98f2ecf21
Sergei <sergei.visotsky@gmail.com>
2018-11-05 17:00:31 +0200

    Minor changes

--- REST-server-spring/sql/rest_services_oauth2_tables.sql
+++ REST-server-spring/sql/rest_services_oauth2_tables.sql
@@ -1,8 +1,6 @@
-DROP DATABASE IF EXISTS rest_services_oauth;
-CREATE DATABASE rest_services_oauth /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+create database if not exists rest_services_oauth;
 
-DROP TABLE IF EXISTS rest_services_oauth.oauth_client_details;
-CREATE TABLE rest_services_oauth.oauth_client_details (
+create table if not exists rest_services_oauth.oauth_client_details (
   client_id               VARCHAR(256) PRIMARY KEY,
   resource_ids            VARCHAR(256),
   client_secret           VARCHAR(256),
@@ -16,8 +14,7 @@ CREATE TABLE rest_services_oauth.oauth_client_details (
   autoapprove             VARCHAR(256)
 );
 
-DROP TABLE IF EXISTS rest_services_oauth.oauth_client_token;
-CREATE TABLE rest_services_oauth.oauth_client_token (
+create table if not exists rest_services_oauth.oauth_client_token (
   token_id          VARCHAR(256),
   token             BLOB,
   authentication_id VARCHAR(256) PRIMARY KEY,
@@ -25,51 +22,32 @@ CREATE TABLE rest_services_oauth.oauth_client_token (
   client_id         VARCHAR(256)
 );
 
-DROP TABLE IF EXISTS rest_services_oauth.oauth_access_token;
-CREATE TABLE rest_services_oauth.oauth_access_token (
+create table if not exists rest_services_oauth.oauth_access_token (
   token_id          VARCHAR(256),
   token             BLOB,
-  authentication_id VARCHAR(256),
+  authentication_id VARCHAR(256) PRIMARY KEY,
   user_name         VARCHAR(256),
   client_id         VARCHAR(256),
   authentication    BLOB,
   refresh_token     VARCHAR(256)
 );
 
-DROP TABLE IF EXISTS rest_services_oauth.oauth_refresh_token;
-CREATE TABLE rest_services_oauth.oauth_refresh_token (
+create table if not exists rest_services_oauth.oauth_refresh_token (
   token_id       VARCHAR(256),
   token          BLOB,
   authentication BLOB
 );
 
-DROP TABLE IF EXISTS rest_services_oauth.oauth_code;
-CREATE TABLE rest_services_oauth.oauth_code (
+create table if not exists rest_services_oauth.oauth_code (
   code           VARCHAR(256),
   authentication BLOB
 );
 
-DROP TABLE IF EXISTS rest_services_oauth.oauth_approvals;
-CREATE TABLE rest_services_oauth.oauth_approvals (
+create table if not exists rest_services_oauth.oauth_approvals (
   userId         VARCHAR(256),
   clientId       VARCHAR(256),
   scope          VARCHAR(256),
   status         VARCHAR(10),
   expiresAt      TIMESTAMP,
   lastModifiedAt TIMESTAMP
-);
-
-DROP TABLE IF EXISTS rest_services_oauth.client_details;
-CREATE TABLE rest_services_oauth.client_details (
-  appId                  VARCHAR(256) PRIMARY KEY,
-  resourceIds            VARCHAR(256),
-  appSecret              VARCHAR(256),
-  scope                  VARCHAR(256),
-  grantTypes             VARCHAR(256),
-  redirectUrl            VARCHAR(256),
-  authorities            VARCHAR(256),
-  access_token_validity  INTEGER,
-  refresh_token_validity INTEGER,
-  additionalInformation  VARCHAR(4096),
-  autoApproveScopes      VARCHAR(256)
 );
