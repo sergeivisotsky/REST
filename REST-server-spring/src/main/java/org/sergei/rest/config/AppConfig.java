a466d7a9da5e00bae0b900fe0456b5a3179b01af
Sergei <sergei.visotsky@gmail.com>
2018-11-27 22:34:31 +0200

    File upload fixed

--- REST-server-spring/src/main/java/org/sergei/rest/config/AppConfig.java
+++ REST-server-spring/src/main/java/org/sergei/rest/config/AppConfig.java
@@ -3,6 +3,7 @@ package org.sergei.rest.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 /**
  * @author Sergei Visotsky, 2018
@@ -13,5 +14,14 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(10000000);
+        return multipartResolver;
+
     }
 }
