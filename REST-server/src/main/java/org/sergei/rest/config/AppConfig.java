212ee8fb728e208ea3e66cbb55b79821a6867127
Sergei <sergei.visotsky@gmail.com>
2019-01-04 19:31:27 +0200

    Logging aspect added

--- REST-server/src/main/java/org/sergei/rest/config/AppConfig.java
+++ REST-server/src/main/java/org/sergei/rest/config/AppConfig.java
@@ -1,15 +1,23 @@
 package org.sergei.rest.config;
 
 import org.modelmapper.ModelMapper;
+import org.sergei.rest.aop.logging.LoggingAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 /**
  * @author Sergei Visotsky
  */
 @Configuration
+@EnableAspectJAutoProxy
 public class AppConfig {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
 
     @Bean
     public ModelMapper modelMapper() {
