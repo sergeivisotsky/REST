dec65ecc21272068776458fc34f4527b0fda1fdd
Sergei <sergei.visotsky@gmail.com>
2019-01-07 15:14:39 +0200

    password encoder in authorization added, another test case added

--- REST-server/src/main/java/org/sergei/rest/security/AuthorizationServerConfig.java
+++ REST-server/src/main/java/org/sergei/rest/security/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -27,22 +28,28 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private final UserApprovalHandler userApprovalHandler;
     private final TokenStore tokenStore;
     private final ApiUserDetailsService apiUserDetailsService;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public AuthorizationServerConfig(DataSource dataSource, UserApprovalHandler userApprovalHandler,
-                                     @Qualifier("authenticationManagerBean") AuthenticationManager authenticationManager,
-                                     TokenStore tokenStore, ApiUserDetailsService apiUserDetailsService) {
+    public AuthorizationServerConfig(DataSource dataSource,
+                                     UserApprovalHandler userApprovalHandler,
+                                     @Qualifier("authenticationManagerBean")
+                                             AuthenticationManager authenticationManager,
+                                     TokenStore tokenStore,
+                                     ApiUserDetailsService apiUserDetailsService,
+                                     BCryptPasswordEncoder passwordEncoder) {
         this.dataSource = dataSource;
         this.authenticationManager = authenticationManager;
         this.userApprovalHandler = userApprovalHandler;
         this.tokenStore = tokenStore;
         this.apiUserDetailsService = apiUserDetailsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     // All the clients are stored into the database
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.jdbc(dataSource);
+        clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
     }
 
     @Override
@@ -56,6 +63,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
-        oauthServer.realm("API_REALM");
+        oauthServer.realm("API_REALM")
+                // Applied for [/oauth/token_key]
+                .tokenKeyAccess("permitAll()")
+                // Applied for [/oauth/check_token]
+                .checkTokenAccess("isAuthenticated()")
+                .passwordEncoder(passwordEncoder);
     }
 }
---
+++ REST-server/src/test/java/org/sergei/rest/controller/OrderControllerTest.java
@@ -0,0 +1,138 @@
+package org.sergei.rest.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sergei.rest.RestServerApplication;
+import org.sergei.rest.model.Customer;
+import org.sergei.rest.model.Order;
+import org.sergei.rest.model.OrderDetails;
+import org.sergei.rest.model.Product;
+import org.sergei.rest.repository.CustomerRepository;
+import org.sergei.rest.repository.OrderRepository;
+import org.sergei.rest.repository.ProductRepository;
+import org.sergei.rest.testconfig.ResourceServerConfiguration;
+import org.sergei.rest.testconfig.WebSecurityConfigTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Test for {@link OrderController}
+ *
+ * @author Sergei Visotsky
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RestServerApplication.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
+@AutoConfigureMockMvc
+@WithMockUser(username = "admin", authorities = {"ROLE_ADMIN"})
+@ContextConfiguration(classes = {ResourceServerConfiguration.class, WebSecurityConfigTest.class})
+@EnableJpaRepositories(basePackages = "org.sergei.rest.repository")
+@EntityScan(basePackages = "org.sergei.rest.model")
+public class OrderControllerTest {
+
+    private static final String BASE_URL = "/api/v1/customers";
+    private static final String ORDER_URI = "/orders";
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    public void findAllOrders_thenReturnOk() throws Exception {
+        final String firstName = "John";
+        final String lastName = "Smith";
+        final int age = 20;
+        Customer customer = setupCustomer(firstName, lastName, age);
+
+        final String productCode = "LV_01";
+        final String productName = "apples";
+        final String productLine = "fruits";
+        final String productVendor = "Val Venosta";
+        final BigDecimal price = new BigDecimal(1.20);
+        Product product = setupProduct(productCode, productName, productLine, productVendor, price);
+
+        final LocalDateTime orderDate = LocalDateTime.parse("2018-09-28T22:00:00", FORMATTER);
+        final LocalDateTime requiredDate = LocalDateTime.parse("2018-09-29T22:00:00", FORMATTER);
+        final LocalDateTime shippedDate = LocalDateTime.parse("2018-09-30T22:00:00", FORMATTER);
+        final String status = "pending";
+        Order order = new Order(customer, orderDate, requiredDate, shippedDate, status);
+
+        List<OrderDetails> orderDetails = new LinkedList<>();
+        final int quantityOrdered = 1;
+        final BigDecimal orderPrice = new BigDecimal(1.20);
+        orderDetails.add(new OrderDetails(product, quantityOrdered, orderPrice, order));
+        order.setOrderDetails(orderDetails);
+
+        setupOrder(customer, orderDate, requiredDate, shippedDate, status);
+
+        mvc.perform(
+                get(BASE_URL + "/" + customer.getCustomerId() + ORDER_URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].orderId").isNotEmpty())
+                .andExpect(jsonPath("$[0].customerId").isNotEmpty())
+                .andExpect(jsonPath("$[0].orderDate").value("2018-09-28"))
+                .andExpect(jsonPath("$[0].requiredDate").value("2018-09-29"))
+                .andExpect(jsonPath("$[0].shippedDate").value("2018-09-30"))
+                .andExpect(jsonPath("$[0].status").value(status))
+                .andExpect(jsonPath("$[0].orderDetails").isEmpty())
+                /*.andExpect(jsonPath("$[0].orderDetails[0].productCode").value(productCode))
+                .andExpect(jsonPath("$[0].orderDetails[0].quantityOrdered").value(quantityOrdered))
+                .andExpect(jsonPath("$[0].orderDetails[0].price").value(orderPrice))*/;
+
+    }
+
+    private Order setupOrder(Customer customer, LocalDateTime orderDate,
+                             LocalDateTime requiredDate, LocalDateTime shippedDate, String status) {
+
+        Order order = new Order(customer, orderDate, requiredDate, shippedDate, status);
+        return orderRepository.save(order);
+    }
+
+    private Product setupProduct(String productCode, String productName,
+                                 String productLine, String productVendor, BigDecimal price) {
+        Product product = new Product(productCode, productName, productLine, productVendor, price);
+        return productRepository.save(product);
+    }
+
+    private Customer setupCustomer(String firstName, String lastName, int age) {
+        Customer customer = new Customer();
+
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setAge(age);
+
+        customerRepository.save(customer);
+
+        return customer;
+    }
+}
