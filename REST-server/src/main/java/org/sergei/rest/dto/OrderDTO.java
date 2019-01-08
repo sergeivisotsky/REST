1717e5b64316b32409fd4bdd70d65087e7e9465e
Sergei <sergei.visotsky@gmail.com>
2019-01-08 07:39:54 +0200

    unused imports removed

--- REST-server/src/main/java/org/sergei/rest/dto/OrderDTO.java
+++ REST-server/src/main/java/org/sergei/rest/dto/OrderDTO.java
@@ -1,13 +1,11 @@
 package org.sergei.rest.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.LinkedList;
