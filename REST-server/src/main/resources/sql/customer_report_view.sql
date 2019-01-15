129573efe006cee992f6ce8fd75c3e92cbfbc354
Sergei <sergei.visotsky@gmail.com>
2019-01-15 07:27:22 +0200

    Comment in SQL addded

--- REST-server/src/main/resources/sql/customer_report_view.sql
+++ REST-server/src/main/resources/sql/customer_report_view.sql
@@ -1,19 +1,19 @@
+/*
+* NOTE: MySQL is preferable choice due to its SQL dialect is used in this script
+*/
 CREATE
   ALGORITHM = UNDEFINED
   DEFINER = `root`@`%`
   SQL SECURITY DEFINER
   VIEW `rest_services`.`customer_report_view` AS
-SELECT
-  `c`.`customer_id` AS `customer_id`,
-  `c`.`first_name` AS `first_name`,
-  `c`.`last_name` AS `last_name`,
-  `o`.`order_id` AS `order_id`,
-  `o`.`order_date` AS `order_date`,
-  `o`.`required_date` AS `required_date`,
-  `o`.`shipped_date` AS `shipped_date`,
-  `o`.`status` AS `status`
-FROM
-  (`rest_services`.`customers` `c`
-    JOIN `rest_services`.`orders` `o`)
-WHERE
-  (`o`.`customer_id` = `c`.`customer_id`)
+SELECT `c`.`customer_id`   AS `customer_id`,
+       `c`.`first_name`    AS `first_name`,
+       `c`.`last_name`     AS `last_name`,
+       `o`.`order_id`      AS `order_id`,
+       `o`.`order_date`    AS `order_date`,
+       `o`.`required_date` AS `required_date`,
+       `o`.`shipped_date`  AS `shipped_date`,
+       `o`.`status`        AS `status`
+FROM (`rest_services`.`customers` `c`
+       JOIN `rest_services`.`orders` `o`)
+WHERE (`o`.`customer_id` = `c`.`customer_id`)
